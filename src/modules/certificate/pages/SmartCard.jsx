f9455d2634d8f98448df62de7d730a68cfb43c5d
mohinimraut7 <mohinimraut7@gmail.com>
2026-09-22 16:15:21 +0530

    top spacing changes

--- src/modules/certificate/pages/SmartCard.jsx
+++ src/modules/certificate/pages/SmartCard.jsx
@@ -5552,7 +5552,7 @@ function IconCardField({ icon: Icon, label, value, wrap = false, accent = "green
 function SectionHeading({ children }) {
   return (
     // <div className="col-span-4 flex items-center gap-2 pt-[3px] first:pt-0">
-        <div className="col-span-4 flex items-center gap-2 pt-[5px] first:pt-[2px]">
+        <div className="col-span-4 flex items-center gap-2 pt-[6px] first:pt-[2px]">
       <span className="whitespace-nowrap text-[8.4px] font-bold uppercase tracking-[0.16em] text-[#0B5D30]">
         {children}
       </span>
